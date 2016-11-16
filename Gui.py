6edf6ce0d62108edaf35d2ca2090cf121bfec17c
PorterK <whyisumad@me.com>
2016-11-16 11:57:13 -0500

    Added readme, documented code

--- Gui.py
+++ Gui.py
@@ -1,43 +1,61 @@
+'''
+
+    Pyazo is my final project for CS230 (Computing I)
+
+    More info in the README
+
+'''
+
+#import all the things
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtGui import QPainter, QColor, QFont, QCursor
 from PyQt5.QtCore import Qt
 
+#Doing this the OOP way..
 class Gui(QWidget):
     def __init__(self):
 
+    #Initialize the QApp/QWidget things
         super().__init__()
 
+    #Build the window in a method to keep the init clean
         self.buildWindow()
-        # self.addCanvas()
 
+#Build the window
     def buildWindow(self):
-        self.setWindowTitle('Icon')
 
+    #Set the window title even though it will not be seen
+        self.setWindowTitle('Pyazo')
 
+    #Make the window transparent
         self.setWindowFlags(self.windowFlags() | Qt.FramelessWindowHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
-
+    #Maximize the window
         self.showMaximized()
-
+    #Render the window
         self.show()
 
+#Handle the mouse events below
+
+#press
     def mousePressEvent(self, event):
         print('mouse clicked!')
-
+#release
     def mouseReleaseEvent(self, event):
         print('released!')
 
 
-
+#Main function
 def main():
-
+#Instantiate our app and Gui stuff.
     app = QApplication(sys.argv)
     gui = Gui()
-
+#Make the cursor the "cross cursor" for effect
     app.setOverrideCursor(QCursor(Qt.CrossCursor))
-
+#Exit when our app exits
     sys.exit(app.exec_())
 
+#That one thing that we write for funzies (not really, its important)
 if(__name__ == '__main__'):
     main()
---
+++ README.md
@@ -0,0 +1,23 @@
+# Pyazo
+#### A screenshot application written in Python
+
+##Why?
+I am writing this application as a final project for CS230 (Computing I) at IUPUI. I felt that I should
+create something that I am willing to show off outside of the classroom, hence this repo.
+
+##Usage
+Feel free to use the app, pick through the source, fork and modify.
+
+Detailed usage to come, packaging may change in the near future
+
+##Bugs
+Please report any bugs to the GitHub Issues section-- not to me directly.
+
+##Todo
+
+- [x] Make the app skeleton
+- [ ] Add the graphical elements
+- [ ] Actually save images
+- [ ] Create the backend API for image uploads
+- [ ] Upload images to the backend
+- [ ] Open image in the browser
